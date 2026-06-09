7de4fd3d941d734ac72ebb2853bd29393cdb94a4
Shyamkumar Pandey <shyamkumarpandey@Shyamkumars-MacBook-Air.local>
2026-06-09 20:21:58 +0530

    feat: export Initiate Work Order doctype, custom fields, and property setters as fixtures

--- qgis/hooks.py
+++ qgis/hooks.py
@@ -247,3 +247,15 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+
+
+fixtures = [
+    {
+        "dt": "DocType",
+        "filters": [
+            ["name", "in", ["Initiate Work Order"]]
+        ]
+    },
+    "Custom Field",
+    "Property Setter"
+]
